640829f5d42360c4e5aaed36ac934ef2e70cc4c2
Denis Savchenko <denis.savchenko98@yandex.ru>
2023-09-11 23:05:09 +0300

    Create solution listener

--- src/main/java/org/jacp/kafka/CompetitionSolutionsListener.java
+++ src/main/java/org/jacp/kafka/CompetitionSolutionsListener.java
@@ -1,6 +1,9 @@
 package org.jacp.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.jacp.controller.ProcessingController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +13,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class CompetitionSolutionsListener {
 
+    @Autowired
+    ProcessingController processingController;
+
+    public CompetitionSolutionsListener(ProcessingController processingController) {
+        this.processingController = processingController;
+    }
+
     @KafkaListener(topics = "${spring.kafka.template.default-topic}",
             groupId = "${spring.kafka.consumer.group-id}")
     public void consume(ConsumerRecord<?, ?> consumerRecord) {
-        System.out.println(consumerRecord.value());
+        String solution = consumerRecord.value().toString();
+        String solutionIdString = solution.substring(solution.indexOf("id=") + 3, solution.indexOf(","));
+        Long solutionId = Long.parseLong(solutionIdString);
+        ResponseEntity<String> response = processingController.getImportAndTest(solutionId);
     }
 }
