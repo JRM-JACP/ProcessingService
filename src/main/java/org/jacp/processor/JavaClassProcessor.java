10f25bc221f18c3a34764d0cc0bcfdfe9f7f5bf4
Denis Savchenko <denis.savchenko98@yandex.ru>
2023-09-28 16:59:29 +0300

    Refactoring JavaClassProcessor. Generate java class

--- src/main/java/org/jacp/processor/JavaClassProcessor.java
+++ src/main/java/org/jacp/processor/JavaClassProcessor.java
@@ -6,22 +6,41 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  * @author saffchen created on 13.09.2023
  */
 @Component
 public class JavaClassProcessor {
-    public void createJavaClass(String imports, String solution, String test) {
-        File file = new File("src/main/java/org/jacp/processor/Upper.java");
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            writer.write("package " + MessageProcessor.class.getPackage().getName() + "\n");
-            writer.write(imports + "\n" + solution);
-            writer.close();
 
+    public String createJavaClass(String imports, String testImports, String className, String testClassName, String solution, String test) {
+        UUID uuid = UUID.randomUUID();
+        String randomPackageName = uuid.toString();
+        String sourcePath = String.format("src/main/java/org/jacp/toResult/%s/source", randomPackageName);
+        String testPath = String.format("src/main/java/org/jacp/toResult/%s/test", randomPackageName);
+        File file = new File(sourcePath);
+        File testFile = new File(testPath);
+        file.mkdirs();
+        testFile.mkdirs();
+        try {
+            String filePath = String.format(file + "/%s.java", className.trim());
+            BufferedWriter sourceClassWriter = new BufferedWriter(new FileWriter(filePath));
+            sourceClassWriter.write("package org.jacp.toResult;");
+            sourceClassWriter.newLine();
+            sourceClassWriter.write(imports + "\n" + solution);
+            sourceClassWriter.close();
+
+            String testFilePath = String.format(testFile + "/%s.java", testClassName.trim());
+            BufferedWriter testClassWriter = new BufferedWriter(new FileWriter(testFilePath));
+            testClassWriter.write("package org.jacp.toCheck;");
+            testClassWriter.newLine();
+            testClassWriter.write(testImports + "import org.jacp.toResult." + className.trim() + ";\n" + test);
+            testClassWriter.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        return randomPackageName;
     }
 }
