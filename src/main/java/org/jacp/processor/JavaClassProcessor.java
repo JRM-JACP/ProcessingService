5fafbd45dff1893f5b585e233a77a1799855f79b
Denis Savchenko <denis.savchenko98@yandex.ru>
2023-09-29 13:13:47 +0300

    Refactoring

--- src/main/java/org/jacp/processor/JavaClassProcessor.java
+++ src/main/java/org/jacp/processor/JavaClassProcessor.java
@@ -17,8 +17,8 @@ public class JavaClassProcessor {
     public String createJavaClass(String imports, String testImports, String className, String testClassName, String solution, String test) {
         UUID uuid = UUID.randomUUID();
         String randomPackageName = uuid.toString();
-        String sourcePath = String.format("src/main/java/org/jacp/toResult/%s/source", randomPackageName);
-        String testPath = String.format("src/main/java/org/jacp/toResult/%s/test", randomPackageName);
+        String sourcePath = String.format("toResult/%s/source", randomPackageName);
+        String testPath = String.format("toResult/%s/test", randomPackageName);
         File file = new File(sourcePath);
         File testFile = new File(testPath);
         file.mkdirs();
@@ -26,16 +26,16 @@ public class JavaClassProcessor {
         try {
             String filePath = String.format(file + "/%s.java", className.trim());
             BufferedWriter sourceClassWriter = new BufferedWriter(new FileWriter(filePath));
-            sourceClassWriter.write("package org.jacp.toResult;");
+            sourceClassWriter.write("package toResult;");
             sourceClassWriter.newLine();
             sourceClassWriter.write(imports + "\n" + solution);
             sourceClassWriter.close();
 
             String testFilePath = String.format(testFile + "/%s.java", testClassName.trim());
             BufferedWriter testClassWriter = new BufferedWriter(new FileWriter(testFilePath));
-            testClassWriter.write("package org.jacp.toCheck;");
+            testClassWriter.write("package toCheck;");
             testClassWriter.newLine();
-            testClassWriter.write(testImports + "import org.jacp.toResult." + className.trim() + ";\n" + test);
+            testClassWriter.write(testImports + "import toResult." + className.trim() + ";\n" + test);
             testClassWriter.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
