c5b78f961f35ae4216b659e800afc7c3cd320766
Denis Savchenko <denis.savchenko98@yandex.ru>
2023-10-04 12:25:17 +0300

    Refactoring

--- src/main/java/org/jacp/processor/ClassCreator.java
+++
@@ -1,8 +0,0 @@
-package org.jacp.processor;
-
-/**
- * @author saffchen created on 04.10.2023
- */
-public interface ClassCreator {
-    void createClass(String imports, String className, String solution);
-}
